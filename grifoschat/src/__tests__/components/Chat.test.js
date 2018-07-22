c9cb5a0d1d0e4bf4556d41d781685153c635796a
William Sergio Almado Jr <william.almado@gmail.com>
2018-07-22 02:48:32 -0300

    finalização dos testes propostos

--- grifoschat/src/__tests__/components/Chat.test.js
+++ grifoschat/src/__tests__/components/Chat.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Chat from '../../components/Chat';
+import { wrap } from 'module';
 
 describe('Message', () => {
     let wrapper;
@@ -32,11 +33,31 @@ describe('Message', () => {
     };
 
     beforeEach(() => {
-        wrapper = shallow(<Chat messages={INITIAL_STATE} />).props();
-
+        wrapper = shallow(<Chat messages={INITIAL_STATE} />);
     })
 
-    it('Rendering simple component', () => {
+    it('Rendering match snapshot', () => {
         expect(Chat).toMatchSnapshot();
+    });
+
+    describe('Rendering props correctly', () => {
+        let component;
+        beforeEach(() => {
+            component = mount(<Chat messages={INITIAL_STATE} />);
+        });
+
+        it('Props must be defined', () => {
+            expect(component.props().messages).toBeDefined();
+        });
+
+        it('Rendering sender with value', () => {
+            expect(component.props().messages.from.messages[0].sender).toBe('Maria');
+            expect(component.props().messages.to.messages[0].sender).toBe('João');
+        });
+
+        it('Text should not be null or empty', () => {
+            expect(component.props().messages.from.messages[0].text.length).toBeGreaterThanOrEqual(1);
+            expect(component.props().messages.to.messages[0].text.length).toBeGreaterThanOrEqual(1);
+        });
     });
 });
